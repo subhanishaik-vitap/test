b566b73bbfe29c25cac02231e0d078aa17658d9d
subhanishaik-vitap <subhani.shaik.vitap@gmail.com>
2026-02-11 21:42:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,48 @@
+require("dotenv").config();
+
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const cors = require("cors");
+const { Mangoclient } = require("mangodb");
+
+const PORT = process.env.PORT;
+const client = new Mangoclient(process.env.DB_URL);
+
+async function ConnectDB() {
+  try {
+    await client.connect();
+    const db = client.db("data");
+    console.log("DB connected..");
+  } catch (err) {
+    console.log("error:", err);
+  }
+}
+
+ConnectDB();
 
 app.use(cors());
 app.use(express.json());
 
-server.listen(3000, "0.0.0.0", () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log("server is running...");
 });
 
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-  res.send("OK!!");
+app.post("/submit", async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { username, password } = req.body;
+    const datasend = await db.collection("users").insertone({
+      username,
+      password,
+    });
+    res.send("OK!!");
+  } catch (err) {
+    console.log("error in saving data:", err);
+    res.status(500).json({ message: "Error saving user" });
+  }
 });
 
 app.get("/", (req, res) => {
